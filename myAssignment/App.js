6913e3d2ed762aa6aed8e7228a6f93be41024b49
Anas-Stasia <anastasiandanwea@gmail.com>
2025-03-20 13:13:33 +0000

    Updated

--- myAssignment/App.js
+++ myAssignment/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import Icon from './assets/person.png'
+import filterIcon  from './assets/filterIcon.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <View styles={styles.texthead}>
+      <View style={styles.texthead}>
         <View>
           <Text style={styles.heading}>Hello , Devs</Text>
           <Text style={styles.subtitle}>14 tasks today</Text>
@@ -15,6 +16,22 @@ export default function App() {
           <Image source={Icon}/>
         </View>
       </View>
+
+      
+        <View  style={styles.subheader}>
+        <TextInput 
+        placeholder='Search'
+        style={styles.TextInput}/>
+
+        <View style={styles.filter}>
+        <Image source={filterIcon}/>
+        </View>
+
+        </View>
+
+       
+       
+     
       
       <StatusBar style="auto" />
     </View>
@@ -51,8 +68,51 @@ const styles = StyleSheet.create({
   },
 
   texthead:{
-    backgroundColor:'black'
+    display:'flex',
+    marginLeft:20,
+    marginRight:19,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+  TextInput:{
+    width: 280,
+    height: 48,
+    backgroundColor:'white',
+    borderRadius: 10,
+    marginLeft:20,
+    fontWeight:'bold',
+    marginTop:30,
+    paddingLeft:45,
+   
+
+  },
+  filter:{
+    width:50,
+    height:48,
+    backgroundColor:'#F0522F',
+    borderRadius:15,
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft:20,
+    marginRight:50
+    
+  
+  
+
+  },
+  subheader:{
+    display:'flex',
+    marginRight:50,
+    marginTop:30,
+    flexDirection:'row',
+    justifyContent:'space-between'
+
   }
+  
+   
+  
   
 
 });
